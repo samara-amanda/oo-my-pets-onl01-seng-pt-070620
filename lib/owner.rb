02e73206c6c59695c0f4de0022c088a57a454f6a
Samara Grant <samara.amanda01@gmail.com>
2020-08-08 21:07:16 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,70 +1,32 @@
 class Owner
-OWNERS = []
-attr_accessor :name, :pets
-attr_reader :species
+  attr_reader :species
+  attr_accessor :name, :pets, :fish, :dog, :cat
 
-def initialize(species)
-  @species = species
-  OWNERS << self
-  @pets = {:fishes => [], :dogs => [], :cats => []}
-end
+  @@all = []
+  @@pets = {:fishes => [], :dogs => [], :cats => []}
 
-def self.all ##Do we have to use this if we use a class variable? @@all
-OWNERS
-end
-
-def self.reset_all
-OWNERS.clear
-end
-
-def self.count
-OWNERS.size
-end
-
-def species
-  @species
-end
-
-def say_species
-  return "I am a #{species}"
-end
-
-def name
-  @name #where did we set @name = name?
-end
-
-def buy_fish(name)
-  pets[:fishes] << Fish.new(name) #how does owner.rb know where fish.rb is?
-end
-
-def buy_cat(name)
-  pets[:cats] << Cat.new(name)
-end
-
-def buy_dog(name)
-  pets[:dogs] << Dog.new(name)
-end
-
-def walk_dogs
-  pets[:dog].each do |dog|
-    dog.mood = "happy"
+  def initialize(species)
+    @species = species
+    @name = name
+    @@all << self
   end
-end
 
-def play_with_cats
-  pets[:cats].each do |cat|
-  cat.mood = "Happy"
+  def self.all
+    @@all
   end
-end
 
-def feed_cat
-  pets[:cat].each do |cat|
-  cat.mood = "happy"
+  def self.count
+    @@all.length
   end
-end
 
-def list_pets
-  "I have #{pets[:fishes].count} fish. I have #{pets[:cats].count} cats. I have #{pets[:dogs].count} dogs."
-end
+  def self.reset_all
+    @@all.clear
+  end
 
-end
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def pets
+    @@pets
+  end
