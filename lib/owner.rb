79fc3172d2c7fc2ab04b3ed367f240604feadc9e
Samara Grant <samara.amanda01@gmail.com>
2020-08-08 23:19:38 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -65,10 +65,13 @@ class Owner
   def sell_pets
     cats.each do |cat|
       cat.mood = "nervous"
+      cat.owner = nil
     end
     dogs.each do |dog|
       dog.mood = "nervous"
+      dog.owner = nil
     end
+
   end
 
   def list_pets
