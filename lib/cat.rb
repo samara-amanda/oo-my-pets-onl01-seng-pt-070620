35f2e7e5c87dbb1f195a482edc495aaf87ccbbfd
Samara Grant <samara.amanda01@gmail.com>
2020-08-08 21:06:44 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,15 @@
 class Cat
+  # code goes here
+  attr_accessor :mood, :owner
   attr_reader :name
-  attr_accessor :mood
-
-  def initialize(name)
+  @@all = []
+  def initialize(name, owner)
     @name = name
+    @owner = owner
     @mood = "nervous"
+    @@all << self
+  end
+  def self.all
+    @@all
   end
 end
