07951523f2a6698af9713c663a5b950f204488ab
Samara Grant <samara.amanda01@gmail.com>
2020-08-08 22:42:57 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -50,13 +50,13 @@ class Owner
 
   def walk_dogs
     @pets[:dogs].each do |dog|
-      dog.mood == "happy"
+      mood = "happy"
     end
   end
 
   def feed_cats
     @pets[:cats].each do |cat|
-      cat.mood == 'happy'
+      mood = 'happy'
     end
   end
 
